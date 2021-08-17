5dc48c4348e65071bfc13fd4baa982d9cc247428
Gabriel Dal Bó <gabrielpdalbo@gmail.com>
2021-08-17 03:46:54 -0300

    views directory create

--- src/server.js
+++ src/server.js
@@ -5,12 +5,10 @@ const server = express()
 
 // request, response
 server.get('/', (req, res) => {
-  return res.render('./index.html')
+  return res.sendFile(__dirname + '/views/index.html')
 })
 
 // Roda o servidor na porta 3000
 server.listen(3000, () => {
   console.log('rodando')
 })
-
-console.log(server)
