c6bd135b531484ad299c5b7c15caea6f0075f935
Gabriel Dal Bó <gabrielpdalbo@gmail.com>
2021-08-17 04:30:29 -0300

    routes update to ejs template engine

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,9 @@
 const express = require('express')
 const routes = express.Router()
 
-const basePath = __dirname + '/views'
-
-routes.get('/', (req, res) => res.sendFile(basePath + '/index.html'))
-routes.get('/job', (req, res) => res.sendFile(basePath + '/job.html'))
-routes.get('/job/edit', (req, res) => res.sendFile(basePath + '/job-edit.html'))
-routes.get('/profile', (req, res) => res.sendFile(basePath + '/profile.html'))
+routes.get('/', (req, res) => res.render('index'))
+routes.get('/job', (req, res) => res.render('job'))
+routes.get('/job/edit', (req, res) => res.render('job-edit'))
+routes.get('/profile', (req, res) => res.render('profile'))
 
 module.exports = routes
