283881219c16e844a1e0a07cc207217fc62c4107
Gabriel Dal Bó <gabrielpdalbo@gmail.com>
2021-08-21 08:12:53 -0300

    dashboard header update

--- src/controllers/DashboardController.js
+++ src/controllers/DashboardController.js
@@ -7,9 +7,24 @@ module.exports = {
     const jobs = Job.get()
     const profile = Profile.get()
 
+    let statusCount = {
+      progress: 0,
+      done: 0,
+      total: jobs.length
+    }
+
+    let jobTotalHours = 0
+
     const updatedJobs = jobs.map(job => {
       const remaining = JobUtils.remainingDays(job)
       const status = remaining <= 0 ? 'done' : 'progress'
+
+      statusCount[status] += 1
+
+      jobTotalHours =
+        status == 'progress'
+          ? jobTotalHours + Number(job['daily-hours'])
+          : jobTotalHours
 
       return {
         ...job,
@@ -18,6 +33,15 @@ module.exports = {
         budget: JobUtils.calculateBudget(job, profile['value-hour'])
       }
     })
-    return res.render('index', { jobs: updatedJobs, profile })
+
+    // qtd de horas que quero trabalhar - qtd de horas/dia de cada job em progresso
+    const freeHours = profile['hours-per-day'] - jobTotalHours
+
+    return res.render('index', {
+      jobs: updatedJobs,
+      profile,
+      statusCount,
+      freeHours
+    })
   }
 }
