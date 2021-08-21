f925d5318e95af40c75c2a32832c9ccf307e2ebc
Gabriel Dal Bó <gabrielpdalbo@gmail.com>
2021-08-21 06:30:53 -0300

    jobUtils.js create

--- src/routes.js
+++ src/routes.js
@@ -97,25 +97,6 @@ const Job = {
 
       return res.redirect('/')
     }
-  },
-  services: {
-    remainingDays(job) {
-      const remainingDays = (job['total-hours'] / job['daily-hours']).toFixed()
-
-      const createdDate = new Date(job.created_at)
-      const dueDay = createdDate.getDate() + Number(remainingDays)
-      const dueDateInMs = createdDate.setDate(dueDay)
-
-      const timeDiffInMs = dueDateInMs - Date.now()
-      // transformar ms em dias
-      const dayInMs = 1000 * 60 * 60 * 24
-      const dayDiff = Math.floor(timeDiffInMs / dayInMs)
-
-      return dayDiff
-    },
-    calculateBudget: (job, valueHour) => {
-      return valueHour * job['total-hours']
-    }
   }
 }
 
