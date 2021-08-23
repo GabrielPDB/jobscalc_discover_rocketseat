a104ea9d25191912c0705f80ce9834b1b66a34ff
Gabriel Dal Bó <gabrielpdalbo@gmail.com>
2021-08-23 20:38:11 -0300

    Profile ok

--- src/model/Profile.js
+++ src/model/Profile.js
@@ -1,4 +1,6 @@
-let data = {
+const Database = require('../db/config')
+
+/* let data = {
   name: 'Gabriel',
   avatar: 'https://github.com/GabrielPDB.png',
   'monthly-budget': 3000,
@@ -6,13 +8,40 @@ let data = {
   'hours-per-day': 5,
   'vacation-per-year': 4,
   'value-hour': 75
-}
+} */
 
 module.exports = {
-  get() {
-    return data
+  async get() {
+    const db = await Database()
+
+    const data = await db.get(`SELECT * FROM profile`)
+
+    await db.close()
+
+    return {
+      name: data.name,
+      avatar: data.avatar,
+      'monthly-budget': data.monthly_budget,
+      'hours-per-day': data.hours_per_day,
+      'days-per-week': data.days_per_week,
+      'vacation-per-year': data.vacation_per_year,
+      'value-hour': data.value_hour
+    }
   },
-  update(newData) {
-    data = newData
+  async update(newData) {
+    const db = await Database()
+
+    db.run(`
+    UPDATE profile SET 
+      name = '${newData.name}',
+      avatar = '${newData.avatar}',
+      monthly_budget = ${newData['monthly-budget']},
+      days_per_week = ${newData['days-per-week']},
+      hours_per_day = ${newData['hours-per-day']},
+      vacation_per_year = ${newData['vacation-per-year']},
+      value_hour = ${newData['value-hour']}
+    `)
+
+    await db.close()
   }
 }
